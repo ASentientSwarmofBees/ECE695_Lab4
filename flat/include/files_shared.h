44a3cfc897e3d2936c086810a21a810ee002cecf
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-17 13:39:48 -0500

    let's make a circular dependency

--- flat/include/files_shared.h
+++ flat/include/files_shared.h
@@ -1,6 +1,8 @@
 #ifndef __FILES_SHARED__
 #define __FILES_SHARED__
 
+#include "dfs_shared.h"
+
 #define FILE_SEEK_SET 1
 #define FILE_SEEK_END 2
 #define FILE_SEEK_CUR 3
@@ -12,8 +14,8 @@
 typedef struct file_descriptor {
   // STUDENT: put file descriptor info here
   uint32 inUse; //an in use indicator to tell if the descriptor is free or in use. Boolean.
-  dfs_inode inode; //inode, which this file-descriptor corresponds to. TODO: should this be a pointer?
   char fileName[FILE_MAX_FILENAME_LENGTH]; //the filename, which is just a string.
+  dfs_inode inode; //inode, which this file-descriptor corresponds to. TODO: should this be a pointer?
   uint32 eof; //eof: Indicator if the End-of-file is reached; useful for read operations. TODO: should be a boolean?
   char mode; //mode: set while opening the file. Possible values: "r", "w" 
   uint32 currentPosition; //current position: Current position in the file. block num or something?
