87b2826147fe70e9438b9614aaa05f77c3e3497f
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-19 14:59:36 -0500

    ostests for dfsallocateblock and dfsfreeblock.

--- flat/include/os/dfs.h
+++ flat/include/os/dfs.h
@@ -20,5 +20,6 @@ int DfsInodeWriteBytes(uint32 handle, void *mem, int start_byte, int num_bytes);
 uint32 DfsInodeFilesize(uint32 handle);
 uint32 DfsInodeAllocateVirtualBlock(uint32 handle, uint32 virtual_blocknum);
 uint32 DfsInodeTranslateVirtualToFilesys(uint32 handle, uint32 virtual_blocknum);
+int CheckIfBlockAllocatedInFBV(uint32 blocknum);
 
 #endif
--- flat/os/ostests.c
+++ flat/os/ostests.c
@@ -7,6 +7,31 @@
 void RunOSTests() {
   // STUDENT: run any os-level tests here
   int i;
+  uint32 array[10];
+
   printf("OS Tests Start\n");
+  printf("Testing DFS Allocate Block.\n");
+  printf("   Checking allocation of 0-9.\n");
+  for (i = 0; i < 10; i++) {
+    printf("   Block %d status: %d.\n", i, CheckIfBlockAllocatedInFBV(i));
+  }
+  printf("   Attempting to allocate 10 blocks.\n");
+  for (i = 0; i < 10; i++) {
+    array[i] = DfsAllocateBlock();
+    printf("   Allocated block %d of 10. Block # %d.\n", i, array[i]);
+  }
+  printf("   Checking allocation of those 10 blocks.\n");
+  for (i = 0; i < 10; i++) {
+    printf("   Block %d status: %d.\n", array[i], CheckIfBlockAllocatedInFBV(array[i]));
+  }
+  printf("   Attempting to free 10 previously allocated blocks.\n");
+  for (i = 0; i < 10; i++) {
+    DfsFreeBlock(array[i]);
+    printf("   Freed block %d of 10. Block # %d.\n", i, array[i]);
+  }
+  printf("   Checking allocation of those 10 blocks.\n");
+  for (i = 0; i < 10; i++) {
+    printf("   Block %d status: %d.\n", array[i], CheckIfBlockAllocatedInFBV(array[i]));
+  }
 }
 
