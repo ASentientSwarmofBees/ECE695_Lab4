e896826d58940278367c089ddcf3c059c3ebc251
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-25 17:24:34 -0500

    sanity check

--- flat/os/dfs.c
+++ flat/os/dfs.c
@@ -391,6 +391,7 @@ uint32 DfsInodeOpen(char *filename) {
 
     for (i = 0; i < sb.numberInodes; i++) {
         LockHandleAcquire(inodeLock);
+        printf("sanity check: handle %d inuse = %d\n", i, inodes[i].inUse);
         if (inodes[i].inUse == 0) {
             // Free inode found
             handle = i;
