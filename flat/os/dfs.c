0853b3239ae7983ec1fad4f15dfc6b81cd8539f6
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-25 14:52:25 -0500

    why isn't ^ or ~ working

--- flat/os/dfs.c
+++ flat/os/dfs.c
@@ -237,6 +237,7 @@ uint32 DfsAllocateBlock() {
 //-----------------------------------------------------------------
 
 int DfsFreeBlock(uint32 blocknum) {
+    uint32 temp;
 
     if (sb.fileSystemValid != 1) {
         return DFS_FAIL;
@@ -253,7 +254,8 @@ int DfsFreeBlock(uint32 blocknum) {
         printf("Attempting to evaluate fbv[%d] ", blocknum / 32);
         printf("[blocknum mod 32 : %d]", blocknum % 32);
         printf("[0x1 << (blocknum mod 32) : 0x%x]", 0x1 << (blocknum % 32));
-        printf("[~(0x1 << (blocknum mod 32)) : 0x%x]", (0x1 << (blocknum % 32)) ^ 0xFFFFFFFF);
+        temp = 0x1 << (blocknum % 32);
+        printf("[~(0x1 << (blocknum mod 32)) : 0x%x]", temp ^ 0xFFFFFFFF);
         printf("&= 0x%x", (0x1 << (blocknum % 32)) ^ 0xFFFFFFFF);
         fbv[blocknum / 32] &= ((0x1 << (blocknum % 32)) ^ 0xFFFFFFFF);
         printf("DfsFreeBlock: Deallocated fs block %d.\n", blocknum);
