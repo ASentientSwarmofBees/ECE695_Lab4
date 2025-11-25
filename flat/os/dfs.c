934466c28738ce77ea44792cae0cb1c3ead002b1
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-25 14:49:41 -0500

    switched ~ for ^

--- flat/os/dfs.c
+++ flat/os/dfs.c
@@ -253,9 +253,9 @@ int DfsFreeBlock(uint32 blocknum) {
         printf("Attempting to evaluate fbv[%d] ", blocknum / 32);
         printf("[blocknum mod 32 : %d]", blocknum % 32);
         printf("[0x1 << (blocknum mod 32) : 0x%x]", 0x1 << (blocknum % 32));
-        printf("[~(0x1 << (blocknum mod 32)) : 0x%x]", ~(0x1 << (blocknum % 32)));
-        printf("&= 0x%x", ~(0x1 << (blocknum % 32)));
-        fbv[blocknum / 32] &= ~(0x1 << (blocknum % 32));
+        printf("[~(0x1 << (blocknum mod 32)) : 0x%x]", (0x1 << (blocknum % 32)) ^ 0xFFFFFFFF);
+        printf("&= 0x%x", (0x1 << (blocknum % 32)) ^ 0xFFFFFFFF);
+        fbv[blocknum / 32] &= ((0x1 << (blocknum % 32)) ^ 0xFFFFFFFF);
         printf("DfsFreeBlock: Deallocated fs block %d.\n", blocknum);
         LockHandleRelease(fbvLock);
         return DFS_SUCCESS;
