6f90e0ba629725eaa3cb7e4d200f0a4c7916ba32
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-26 11:15:26 -0500

    wrote test 1 for file tests

--- flat/apps/ostests/ostests/ostests.c
+++ flat/apps/ostests/ostests/ostests.c
@@ -1,6 +1,32 @@
 #include "usertraps.h"
 
+#define RUN_OS_TESTS 0
+
+#define NUM_INTS 300
+
 void main (int argc, char *argv[])
 {
-  run_os_tests();
+  int dataArray1[NUM_INTS];
+  int dataArray2[NUM_INTS];
+  int i, handle1, handle2;
+
+  if (RUN_OS_TESTS == 1) {
+    run_os_tests();
+    return;
+  }
+  Printf("Running File Tests!\n");
+  Printf("File Tests: Test 1: Testing opening a file, writing data to it, and reading back the same data.\n");
+  for (i = 0; i < NUM_INTS; i++) {
+    dataArray1[i] = i;
+  }
+  handle1 = file_open("file_test_1", 'w');
+  file_write(handle1, dataArray1, sizeof(int)*NUM_INTS);
+  file_read(handle1, dataArray2, sizeof(int)*NUM_INTS);
+  for (i = 0; i < NUM_INTS; i++) {
+    if (dataArray1[i] != dataArray2[i]) {
+      Printf("File Tests: Error! d1[%d] != d2[%d] (%d != %d)\n", i, i, dataArray1[i], dataArray2[i]);
+      return;
+    }
+  }
+  Printf("File Tests: Passed Test 1!\n");
 }
