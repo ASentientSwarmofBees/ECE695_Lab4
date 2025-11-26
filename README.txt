e4aa4e90e49fba0a3406285ec780eca5918a18c6
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-25 19:07:15 -0500

    I'm making edits to fdisk, but it really seems like i need a way to invalidate the version of sb that the OS has in memory. I feel like that's why it's getting overwritten.

--- README.txt
+++ README.txt
@@ -10,7 +10,7 @@ PART 3
 Files edited: dfs.c, dfs_shared.h, ostests.c
 
 PART 4
-Files edited: ostests.c, files.c
+Files edited: ostests.c, files.c, fdisk.c
 Sources referenced: https://stackoverflow.com/questions/348170/how-do-i-undo-git-add-before-commit
 https://stackoverflow.com/questions/31281679/how-to-undo-local-changes-to-a-specific-file
 https://stackoverflow.com/questions/6794110/git-revert-back-to-certain-commit
--- flat/apps/fdisk/fdisk/fdisk.c
+++ flat/apps/fdisk/fdisk/fdisk.c
@@ -89,6 +89,7 @@ void main (int argc, char *argv[])
   FdiskWriteFileSystemBlock(1, sb_dfsblock); //fs block 1 is the superblock
   FdiskWriteFileSystemBlock(65535, sb_dfsblock); //copy the superblock also to file system block 65535, where the copy of the superblock goes (physical blocks 262140-262143)
   Printf("fdisk (%d): Formatted DFS disk for 0x%x bytes.\n", getpid(), disksize);
+  DfsInvalidate();
 }
 
 int FdiskWriteFileSystemBlock(uint32 fsblocknum, dfs_block *b) {
